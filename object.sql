a1dfbb501c0a7b1737256a9f7a447737d4b683fb
LuminouZ307 <75291661+LuminouZ307@users.noreply.github.com>
2021-05-14 12:37:43 +0700

    Add files via upload

--- object.sql
+++ object.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2021 at 03:56 AM
+-- Generation Time: May 14, 2021 at 07:37 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `object`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matext`
+--
+
+CREATE TABLE `matext` (
+  `mtID` int(8) NOT NULL,
+  `mtText` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `mtX` float NOT NULL DEFAULT 0,
+  `mtY` float NOT NULL DEFAULT 0,
+  `mtZ` float NOT NULL DEFAULT 0,
+  `mtRX` float NOT NULL DEFAULT 0,
+  `mtRY` float NOT NULL,
+  `mtRZ` float NOT NULL DEFAULT 0,
+  `mtInterior` int(6) NOT NULL DEFAULT 0,
+  `mtWorld` int(6) NOT NULL DEFAULT 0,
+  `mtBold` int(6) NOT NULL DEFAULT 0,
+  `mtColor` int(4) NOT NULL DEFAULT 0,
+  `mtSize` int(12) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `matext`
+--
+
+INSERT INTO `matext` (`mtID`, `mtText`, `mtX`, `mtY`, `mtZ`, `mtRX`, `mtRY`, `mtRZ`, `mtInterior`, `mtWorld`, `mtBold`, `mtColor`, `mtSize`) VALUES
+(2, 'Testis', 1962.08, 1339.3, 9.2578, 0, 0, 84.1461, 0, 0, 0, 1, 20),
+(3, 'HALO ANJ', 1961.78, 1330.13, 8.3878, 0, 0, 128.821, 0, 0, 0, 2, 30);
 
 -- --------------------------------------------------------
 
@@ -41,8 +71,22 @@ CREATE TABLE `object` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `object`
+--
+
+INSERT INTO `object` (`objid`, `objectModel`, `objectX`, `objectY`, `objectZ`, `objectRX`, `objectRY`, `objectRZ`, `objectInterior`, `objectWorld`) VALUES
+(8, 4642, 1961.82, 1343.91, 9.7446, 0, 0, 92.9195, 0, 0),
+(9, 1642, 1961.94, 1331.59, 9.2578, 0, 0, 179.921, 0, 0);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `matext`
+--
+ALTER TABLE `matext`
+  ADD PRIMARY KEY (`mtID`);
 
 --
 -- Indexes for table `object`
@@ -55,10 +99,16 @@ ALTER TABLE `object`
 --
 
 --
+-- AUTO_INCREMENT for table `matext`
+--
+ALTER TABLE `matext`
+  MODIFY `mtID` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `object`
 --
 ALTER TABLE `object`
-  MODIFY `objid` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `objid` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
